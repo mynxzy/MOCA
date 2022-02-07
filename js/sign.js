64e882569889837e966fc4f29c3548338a6f7b31
mynxzy <88770029+mynxzy@users.noreply.github.com>
2022-02-07 16:11:19 +0900

    no message

---
+++ js/sign.js
@@ -0,0 +1,10 @@
+var u_name = document.getElementById('userName');
+var u_id = document.getElementById('userId');
+var u_pw = document.getElementById('userPw');
+var u_pwChk = document.getElementById('userPwChk');
+var u_nick = document.getElementById('nickName');
+var u_addr = document.getElementById('address');
+
+function (){
+
+}
